4781a012c0b4fc4ce785ce2ac2c8e9a1630c568c
LaurensGiesen <77290061+LaurensGiesen@users.noreply.github.com>
2021-09-25 00:55:15 +0200

    Refactor main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from termcolor import cprint
-
 #  | | | | | |
 # -------------
 #  | | | | | |
@@ -13,7 +12,6 @@ from termcolor import cprint
 #  | | | | | |
 # -------------
 #  | | | | | |
-
 field = [[" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "], [" ", " ", " ", " ", " ", " ", " "],
          [" ", " ", " ", " ", " ", " ", " "]]
@@ -58,78 +56,31 @@ def walk_over_col_slant_by_right_check(board_field, start_col, stop_col, start_r
         start_col -= 1
         start_row += 1
 
-
 def winner_slant(board_field):
     #left_check
     list_slant_x = []
     list_slant_0 = []
-    start_col3 = 3
-    stop_col6 = 6
-    start_row0 = 0
-    if walk_over_every_col_slant_by_left_check(board_field, start_col3, stop_col6, start_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_col2 = 2
-    if walk_over_every_col_slant_by_left_check(board_field, start_col2, stop_col6, start_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_col1 = 1
-    if walk_over_every_col_slant_by_left_check(board_field, start_col1, stop_col6, start_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_col0 = 0
-    if walk_over_every_col_slant_by_left_check(board_field, start_col0, stop_col6, start_row0, list_slant_x, list_slant_0):
-        return True
-
-    stop_col5 = 5
-    start_row1 = 1
-    if walk_over_every_col_slant_by_left_check(board_field, start_col0, stop_col5, start_row1, list_slant_x, list_slant_0):
-        return True
-
-    stop_col4 = 4
-    start_row2 = 2
-    if walk_over_every_col_slant_by_left_check(board_field, start_col0, stop_col4, start_row2, list_slant_x, list_slant_0):
-        return True
-
-    stop_col3 = 3
-    start_row3 = 3
-    if walk_over_every_col_slant_by_left_check(board_field, start_col0, stop_col3, start_row3, list_slant_x, list_slant_0):
-        return True
-
+    for start_col in range(3, -1, -1):
+        if walk_over_every_col_slant_by_left_check(board_field, start_col, 6, 0, list_slant_x,
+                                                   list_slant_0):
+            return True
+    start_row = 0
+    for stop_col in range(5, 2, -1):
+        start_row += 1
+        if walk_over_every_col_slant_by_left_check(board_field, 0, stop_col, start_row, list_slant_x,
+                                                   list_slant_0):
+            return True
     #right_check
-    start_right_col3 = 3
-    stop_right_col0 = 0
-    start_right_row0 = 0
-    if walk_over_col_slant_by_right_check(board_field, start_right_col3, stop_right_col0, start_right_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_right_col4 = 4
-    if walk_over_col_slant_by_right_check(board_field, start_right_col4, stop_right_col0, start_right_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_right_col5 = 5
-    stop_right_col0 = 0
-    if walk_over_col_slant_by_right_check(board_field, start_right_col5, stop_right_col0, start_right_row0, list_slant_x, list_slant_0):
-        return True
-
-    start_right_col6 = 6
-    if walk_over_col_slant_by_right_check(board_field, start_right_col6, stop_right_col0, start_right_row0, list_slant_x, list_slant_0):
-        return True
-
-    stop_right_col1 = 1
-    start_right_row1 = 1
-    if walk_over_col_slant_by_right_check(board_field, start_right_col6, stop_right_col1, start_right_row1, list_slant_x, list_slant_0):
-        return True
-
-    stop_right_col2 = 2
-    start_right_row2 = 2
-    if walk_over_col_slant_by_right_check(board_field, start_right_col6, stop_right_col2, start_right_row2, list_slant_x, list_slant_0):
-        return True
-
-    stop_right_col3 = 3
-    start_right_row3 = 3
-    if walk_over_col_slant_by_right_check(board_field, start_right_col6, stop_right_col3, start_right_row3, list_slant_x, list_slant_0):
-        return True
+    for start_right_col in range(3, 7):
+        if walk_over_col_slant_by_right_check(board_field, start_right_col, 0, 0,
+                                              list_slant_x, list_slant_0):
+            return True
+    start_right_row = 0
+    for stop_right_col in range(1, 4):
+        start_right_row += 1
+        if walk_over_col_slant_by_right_check(board_field, 6, stop_right_col, start_right_row,
+                                              list_slant_x, list_slant_0):
+            return True
 
 def walk_over_every_col_horizontal_check(board_field, board_row, list1, list2):
     list1.clear()
@@ -152,27 +103,9 @@ def walk_over_every_col_horizontal_check(board_field, board_row, list1, list2):
 def winner_horizontal(board_field):
     horizontal_list_x = []
     horizontal_list_o = []
-    row6 = 6
-    if walk_over_every_col_horizontal_check(board_field, row6, horizontal_list_x, horizontal_list_o):
-        return True
-    row5 = 5
-    if walk_over_every_col_horizontal_check(board_field, row5, horizontal_list_x, horizontal_list_o):
-        return True
-    row4 = 4
-    if walk_over_every_col_horizontal_check(board_field, row4, horizontal_list_x, horizontal_list_o):
-        return True
-    row3 = 3
-    if walk_over_every_col_horizontal_check(board_field, row3, horizontal_list_x, horizontal_list_o):
-        return True
-    row2 = 2
-    if walk_over_every_col_horizontal_check(board_field, row2, horizontal_list_x, horizontal_list_o):
-        return True
-    row1 = 1
-    if walk_over_every_col_horizontal_check(board_field, row1, horizontal_list_x, horizontal_list_o):
-        return True
-    row0 = 0
-    if walk_over_every_col_horizontal_check(board_field, row0, horizontal_list_x, horizontal_list_o):
-        return True
+    for position in range(6, -1, -1):
+        if walk_over_every_col_horizontal_check(board_field, position, horizontal_list_x, horizontal_list_o):
+            return True
 
 def winner_vertical(board_field):
     list_x = []
@@ -210,28 +143,10 @@ def winner_vertical(board_field):
                     else:
                         continue
 
-def return_row(board_field, col):
-    row6 = 6
-    if board_field[col][row6] == " ":
-        return row6
-    row5 = 5
-    if board_field[col][row5] == " ":
-        return row5
-    row4 = 4
-    if board_field[col][row4] == " ":
-        return row4
-    row3 = 3
-    if board_field[col][row3] == " ":
-        return row3
-    row2 = 2
-    if board_field[col][row2] == " ":
-        return row2
-    row1 = 1
-    if board_field[col][row1] == " ":
-        return row1
-    row0 = 0
-    if board_field[col][row0] == " ":
-        return row0
+def return_empty_row(board_field, col):
+    for row_position in range(6, -1, -1):
+        if board_field[col][row_position] == " ":
+            return row_position
 
 def draw_connect_4_board(board_field):
     for row in range(13):
@@ -254,12 +169,13 @@ def check_col(col):
 
 draw_connect_4_board(field)
 player = 1
+
 while True:
     if player == 1:
         column_player = int(input("Player 1 - Column: "))
         column_player -= 1
         if check_col(column_player):
-            row = return_row(field, column_player)
+            row = return_empty_row(field, column_player)
             if row is not None:
                 field[column_player][row] = "X"
                 if winner_vertical(field):
@@ -281,7 +197,7 @@ while True:
         column_player = int(input("Player 2 - Column: "))
         column_player -= 1
         if check_col(column_player):
-            row1 = return_row(field, column_player)
+            row1 = return_empty_row(field, column_player)
             if row1 is not None:
                 field[column_player][row1] = "O"
                 if winner_vertical(field):
@@ -300,4 +216,3 @@ while True:
             else:
                 print("You can't put more in here")
     draw_connect_4_board(field)
-
